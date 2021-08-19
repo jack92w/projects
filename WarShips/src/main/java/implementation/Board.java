4ee2c55f9a1e09ef1436a91a99e5719ea622f44d
jacek <jacek.hrynczyszyn@gmail.com>
2021-08-19 15:10:10 +0200

    Coordinate iterator was prepared

--- WarShips/src/main/java/implementation/Board.java
+++ WarShips/src/main/java/implementation/Board.java
@@ -1,6 +1,47 @@
 package implementation;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class Board {
+
+    private int[][] tab;
+
+    static final int SIZE = 10;
+
+    Coordinate coordinate;
+
+    public Board() {
+        tab = new int[SIZE][SIZE];
+    }
+    public BoardIterator createIterator(){
+        return new BoardIterator();
+    }
+
+    class BoardIterator implements Iterator<Coordinate> {
+
+        Coordinate currentCoordinate = new Coordinate(-1, 0);
+
+        @Override
+        public boolean hasNext() {
+            return currentCoordinate.canMoveRight() || currentCoordinate.canMoveDown();
+        }
+
+        @Override
+        public Coordinate next() {
+            if(hasNext()){
+                if (currentCoordinate.canMoveRight()) {
+                    currentCoordinate.setX(currentCoordinate.getX()+1);
+                }
+                else if(currentCoordinate.canMoveDown()){
+                    currentCoordinate.setX(0);
+                    currentCoordinate.setY(currentCoordinate.getY()+1);
+                }
+                return currentCoordinate;
+            }
+            throw new NoSuchElementException("end of board");
+        }
+    }
 
 
 }
