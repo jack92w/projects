2c9b62ed1630009a1ca8d9c507a178435916b6bb
jacek <jacek.hrynczyszyn@gmail.com>
2021-08-19 17:17:25 +0200

    Methods for ships  creation were prepared

--- WarShips/src/main/java/implementation/Coordinate.java
+++ WarShips/src/main/java/implementation/Coordinate.java
@@ -10,6 +10,38 @@ public class Coordinate {
         this.y = y;
     }
 
+    protected boolean canMoveRight() {
+        return x < Board.SIZE - 1;
+    }
+
+    protected boolean canMoveTwoRight() {
+        return x < Board.SIZE - 2;
+    }
+
+    protected boolean canMoveThreeRight() {
+        return x < Board.SIZE - 3;
+    }
+
+    protected boolean canMoveFourRight() {
+        return x < Board.SIZE - 4;
+    }
+
+    protected boolean canMoveDown() {
+        return y < Board.SIZE - 1;
+    }
+
+    protected boolean canTwoMoveDown() {
+        return y < Board.SIZE - 2;
+    }
+
+    protected boolean canThreeMoveDown() {
+        return y < Board.SIZE - 3;
+    }
+
+    protected boolean canFourMoveDown() {
+        return y < Board.SIZE - 4;
+    }
+
     public int getX() {
         return x;
     }
