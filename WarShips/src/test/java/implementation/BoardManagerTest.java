7a8ec98c238ab953a5d3d1038fe9ec262481100f
jacek <jacek.hrynczyszyn@gmail.com>
2021-09-14 17:57:18 +0200

    tests for confirmIfFieldsAreZero method have been prepared

--- WarShips/src/test/java/implementation/BoardManagerTest.java
+++ WarShips/src/test/java/implementation/BoardManagerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,76 +12,110 @@ class BoardManagerTest {
     private BoardManager boardManager;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         boardManager = new BoardManager();
     }
 
     @Test
-    void shouldCreateBoard_andPrint(){
+    void shouldCreateBoard_andPrint() {
         boardManager.initBoard();
         boardManager.printBoard();
         System.out.println();
     }
 
     @Test
-    void shouldReturnProperSetOfHorizontalCoordinate(){
+    void shouldReturnProperSetOfHorizontalCoordinate() {
         //given
-        Coordinate coordinate = new Coordinate(4,5);
+        Coordinate coordinate = new Coordinate(4, 5);
         Set<Coordinate> expectedSet = new HashSet<>();
-        expectedSet.add(new Coordinate(4,5));
-        expectedSet.add(new Coordinate(5,5));
-        expectedSet.add(new Coordinate(6,5));
+        expectedSet.add(new Coordinate(4, 5));
+        expectedSet.add(new Coordinate(5, 5));
+        expectedSet.add(new Coordinate(6, 5));
         //when
-        Set<Coordinate> actualSet =  boardManager.createCoordinateSetHorizontal(coordinate, 3);
+        Set<Coordinate> actualSet = boardManager.createCoordinateSet(coordinate, 3, Plane.HORIZONTAL);
         //then
         Assertions.assertTrue(expectedSet.containsAll(actualSet));
         Assertions.assertEquals(3, actualSet.size());
     }
 
     @Test
-    void shouldThrowIllegalArgumentExceptionIfMastQuantityLessThanOne(){
+    void shouldThrowIllegalArgumentExceptionIfMastQuantityLessThanOne() {
         //given
-        Coordinate coordinate = new Coordinate(4,5);
+        Coordinate coordinate = new Coordinate(4, 5);
         Set<Coordinate> expectedSet = new HashSet<>();
         //when
         //then
-        Assertions.assertThrows(IllegalArgumentException.class, ()-> boardManager.createCoordinateSetHorizontal(coordinate, -2));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> boardManager.createCoordinateSet(coordinate, -2, Plane.HORIZONTAL));
     }
 
     @Test
-    void shouldChangeFieldsNumericValue(){
+    void shouldChangeFieldsNumericValue() {
         //given
         int mastQuantity = 3;
-        Coordinate coordinate = new Coordinate(4,5);
+        Coordinate coordinate = new Coordinate(4, 5);
         Set<Coordinate> expectedSet = new HashSet<>();
         expectedSet.add(coordinate);
-        boardManager.board.setElementAt(0, coordinate);
+        boardManager.board.setNumberAt(0, coordinate);
         //when
         boardManager.changeFields(expectedSet, mastQuantity);
         //then
-        Assertions.assertEquals(mastQuantity, boardManager.board.getElementAt(coordinate));
+        Assertions.assertEquals(mastQuantity, boardManager.board.getNumberAt(coordinate));
     }
 
     @Test
-    void shouldReturnTruIfCanMoveRight(){
+    void shouldReturnTruIfCanMoveRight() {
         //given
-        Coordinate coordinate = new Coordinate(4,5);
+        Coordinate coordinate = new Coordinate(4, 5);
         //when
-        boolean actual = boardManager.baseOnMastQuantityConfirmIfCanInsertShipRight(coordinate,4);
+        boolean actual = boardManager.baseOnMastQuantityConfirmIfCanInsertShip(coordinate, 4, Plane.HORIZONTAL);
         //then
         Assertions.assertTrue(actual);
     }
 
     @Test
-    void shouldReturnFalseIfCanNotMoveRight(){
+    void shouldReturnFalseIfCanNotMoveRight() {
         //given
-        Coordinate coordinate = new Coordinate(7,5);
+        Coordinate coordinate = new Coordinate(7, 5);
         //when
-        boolean actual = boardManager.baseOnMastQuantityConfirmIfCanInsertShipRight(coordinate,4);
+        boolean actual = boardManager.baseOnMastQuantityConfirmIfCanInsertShip(coordinate, 4, Plane.HORIZONTAL);
         //then
         Assertions.assertFalse(actual);
     }
 
+    @Test
+    void shouldReturnFalseIfInCoordinateSetIsNumberDifferentThanZero() {
+        //given
+        Set<Coordinate> set = new HashSet<>();
+        Coordinate coordinate1 = new Coordinate(7, 5);
+        Coordinate coordinate2 = new Coordinate(8, 5);
+        Coordinate coordinate3 = new Coordinate(9, 5);
+        set.add(coordinate1);
+        set.add(coordinate2);
+        set.add(coordinate3);
+        for (Coordinate coord : set) {
+            boardManager.board.setNumberAt(0, coord);
+        }
+        boardManager.board.setNumberAt(3, coordinate1);
+        //when
+        boolean actual = boardManager.confirmIfFieldsAreZero(set);
+        //then
+        Assertions.assertFalse(actual);
+    }
 
+    @Test
+    void shouldReturnTrueIfInCoordinateSetAreOnlyZeros() {
+        //given
+        Set<Coordinate> set = new HashSet<>();
+        Coordinate coordinate1 = new Coordinate(7, 5);
+        Coordinate coordinate2 = new Coordinate(8, 5);
+        Coordinate coordinate3 = new Coordinate(9, 5);
+        set.add(coordinate1);
+        set.add(coordinate2);
+        set.add(coordinate3);
+        //when
+        boolean actual = boardManager.confirmIfFieldsAreZero(set);
+        //then
+        Assertions.assertTrue(actual);
+    }
 
 }
