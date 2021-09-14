cc4eb7b10c6021b1a45ce5a53f99760ba25ba496
jacek <jacek.hrynczyszyn@gmail.com>
2021-09-14 15:29:19 +0200

    tests for baseOnMastQuantityConfirmIfCanInsertShipRight method have been prepared

--- WarShips/src/test/java/implementation/BoardManagerTest.java
+++ WarShips/src/test/java/implementation/BoardManagerTest.java
@@ -1,7 +1,12 @@
 package implementation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
+import java.util.HashSet;
+import java.util.Set;
 
 class BoardManagerTest {
 
@@ -18,5 +23,66 @@ class BoardManagerTest {
         boardManager.printBoard();
         System.out.println();
     }
+
+    @Test
+    void shouldReturnProperSetOfHorizontalCoordinate(){
+        //given
+        Coordinate coordinate = new Coordinate(4,5);
+        Set<Coordinate> expectedSet = new HashSet<>();
+        expectedSet.add(new Coordinate(4,5));
+        expectedSet.add(new Coordinate(5,5));
+        expectedSet.add(new Coordinate(6,5));
+        //when
+        Set<Coordinate> actualSet =  boardManager.createCoordinateSetHorizontal(coordinate, 3);
+        //then
+        Assertions.assertTrue(expectedSet.containsAll(actualSet));
+        Assertions.assertEquals(3, actualSet.size());
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionIfMastQuantityLessThanOne(){
+        //given
+        Coordinate coordinate = new Coordinate(4,5);
+        Set<Coordinate> expectedSet = new HashSet<>();
+        //when
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> boardManager.createCoordinateSetHorizontal(coordinate, -2));
+    }
+
+    @Test
+    void shouldChangeFieldsNumericValue(){
+        //given
+        int mastQuantity = 3;
+        Coordinate coordinate = new Coordinate(4,5);
+        Set<Coordinate> expectedSet = new HashSet<>();
+        expectedSet.add(coordinate);
+        boardManager.board.setElementAt(0, coordinate);
+        //when
+        boardManager.changeFields(expectedSet, mastQuantity);
+        //then
+        Assertions.assertEquals(mastQuantity, boardManager.board.getElementAt(coordinate));
+    }
+
+    @Test
+    void shouldReturnTruIfCanMoveRight(){
+        //given
+        Coordinate coordinate = new Coordinate(4,5);
+        //when
+        boolean actual = boardManager.baseOnMastQuantityConfirmIfCanInsertShipRight(coordinate,4);
+        //then
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void shouldReturnFalseIfCanNotMoveRight(){
+        //given
+        Coordinate coordinate = new Coordinate(7,5);
+        //when
+        boolean actual = boardManager.baseOnMastQuantityConfirmIfCanInsertShipRight(coordinate,4);
+        //then
+        Assertions.assertFalse(actual);
+    }
+
+
 
 }
