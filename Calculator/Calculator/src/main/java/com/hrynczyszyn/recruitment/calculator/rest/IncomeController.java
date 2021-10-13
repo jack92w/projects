c000d852bbfc512122ecb835f9166c1e7a6ea5ff
jacek <jacek.hrynczyszyn@gmail.com>
2021-10-13 21:49:26 +0200

    post mapping has been prepared

---
+++ Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/rest/IncomeController.java
@@ -0,0 +1,36 @@
+package com.hrynczyszyn.recruitment.calculator.rest;
+
+import com.hrynczyszyn.recruitment.calculator.dto.IncomeDto;
+import com.hrynczyszyn.recruitment.calculator.entity.Income;
+import com.hrynczyszyn.recruitment.calculator.mapper.Mapper;
+import com.hrynczyszyn.recruitment.calculator.repository.IncomeRepository;
+import com.hrynczyszyn.recruitment.calculator.service.IncomeCalculatorService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class IncomeController {
+
+    private IncomeCalculatorService incomeCalculatorService;
+    private IncomeRepository incomeRepository;
+    private Mapper mapper;
+
+    public IncomeController(IncomeCalculatorService incomeCalculatorService, IncomeRepository incomeRepository, Mapper mapper) {
+        this.incomeCalculatorService = incomeCalculatorService;
+        this.incomeRepository = incomeRepository;
+        this.mapper = mapper;
+    }
+
+    @PostMapping("/income_calculation/{income}")
+    public BigDecimal calculateAndSaveIncome(@PathVariable BigDecimal income) {
+        BigDecimal incomeValue = incomeCalculatorService.calculateIncome(income);
+        IncomeDto dto = IncomeDto.builder().
+                incomeAmount(incomeValue).build();
+        Income incomeToBeSaved = mapper.mapToEntity(dto);
+        incomeRepository.save(incomeToBeSaved);
+        return incomeValue;
+    }
+}
