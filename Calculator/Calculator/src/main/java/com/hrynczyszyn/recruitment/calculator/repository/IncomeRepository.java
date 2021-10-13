b1cbc83f4bf8404a167ffb50b23a118783cc854f
jacek <jacek.hrynczyszyn@gmail.com>
2021-10-13 19:45:28 +0200

    IncomeRepository has been prepared

---
+++ Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/repository/IncomeRepository.java
@@ -0,0 +1,10 @@
+package com.hrynczyszyn.recruitment.calculator.repository;
+
+import com.hrynczyszyn.recruitment.calculator.entity.Income;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IncomeRepository extends JpaRepository<Income, Long> {
+
+}
