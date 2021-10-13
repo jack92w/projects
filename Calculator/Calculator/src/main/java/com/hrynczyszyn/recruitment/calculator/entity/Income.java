2f52461e2d161c6dd179c2c14c7571afd55f098a
jacek <jacek.hrynczyszyn@gmail.com>
2021-10-13 19:40:42 +0200

    Lombok functions has been added

--- Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/entity/Income.java
+++ Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/entity/Income.java
@@ -1,10 +1,20 @@
 package com.hrynczyszyn.recruitment.calculator.entity;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Objects;
 
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "income_tbl")
 public class Income {
     @Id
@@ -13,4 +23,17 @@ public class Income {
 
     @Column(name = "income_amount", nullable = false)
     private BigDecimal incomeAmount;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Income income = (Income) o;
+        return Objects.equals(id, income.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
