9ddb09385c5c904830e5dab764b7ab3e11510906
jacek <jacek.hrynczyszyn@gmail.com>
2021-10-13 21:58:17 +0200

    query has been prepared

--- Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/repository/IncomeRepository.java
+++ Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/repository/IncomeRepository.java
@@ -2,9 +2,15 @@ package com.hrynczyszyn.recruitment.calculator.repository;
 
 import com.hrynczyszyn.recruitment.calculator.entity.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+
+    @Query("SELECT income_amount FROM income_tbl ORDER BY id DESC")
+    List<Income> findLastFiveRecords();
 
 }
