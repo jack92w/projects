7e2ddb21b9cea421ff30e9681ba0e2d558688a19
jacek <jacek.hrynczyszyn@gmail.com>
2021-10-15 19:00:42 +0200

    Javadoc has been prepared

--- Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/service/IncomeCalculatorService.java
+++ Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/service/IncomeCalculatorService.java
@@ -4,5 +4,10 @@ import java.math.BigDecimal;
 
 public interface IncomeCalculatorService {
 
+    /**
+     * calculates income
+     * @param moneyAmount input for calculation
+     * @return income
+     */
     BigDecimal calculateIncome(BigDecimal moneyAmount);
 }
