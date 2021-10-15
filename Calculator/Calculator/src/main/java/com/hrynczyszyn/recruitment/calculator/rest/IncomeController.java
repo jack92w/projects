70ebfd08862af6d38b2cd7069913e522e9044cd9
jacek <jacek.hrynczyszyn@gmail.com>
2021-10-15 20:59:53 +0200

    Javadoc has been added

--- Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/rest/IncomeController.java
+++ Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/rest/IncomeController.java
@@ -1,44 +1,39 @@
 package com.hrynczyszyn.recruitment.calculator.rest;
 
-import com.hrynczyszyn.recruitment.calculator.dto.IncomeDto;
-import com.hrynczyszyn.recruitment.calculator.entity.Income;
-import com.hrynczyszyn.recruitment.calculator.mapper.Mapper;
 import com.hrynczyszyn.recruitment.calculator.repository.IncomeRepository;
 import com.hrynczyszyn.recruitment.calculator.service.IncomeCalculatorService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class IncomeController {
 
-    private IncomeCalculatorService incomeCalculatorService;
-    private IncomeRepository incomeRepository;
-    private Mapper mapper;
+    private final IncomeCalculatorService incomeCalculatorService;
+    private final IncomeRepository repository;
 
-    public IncomeController(IncomeCalculatorService incomeCalculatorService, IncomeRepository incomeRepository, Mapper mapper) {
-        this.incomeCalculatorService = incomeCalculatorService;
-        this.incomeRepository = incomeRepository;
-        this.mapper = mapper;
+    /**
+     * calculates income and save it in list
+     * @param rawIncome to be calculated
+     * @return calculated income
+     */
+    @PostMapping("/income/{rawIncome}")
+    public BigDecimal calculateAndSaveIncome(@PathVariable BigDecimal rawIncome) {
+        BigDecimal calculatedIncome = incomeCalculatorService.calculateIncome(rawIncome);
+        repository.saveIncome(calculatedIncome);
+        return calculatedIncome;
     }
 
-    @PostMapping("/income_calculation/{income}")
-    public BigDecimal calculateAndSaveIncome(@PathVariable BigDecimal income) {
-        BigDecimal incomeValue = incomeCalculatorService.calculateIncome(income);
-        IncomeDto dto = IncomeDto.builder().
-                incomeAmount(incomeValue).build();
-        Income incomeToBeSaved = mapper.mapToEntity(dto);
-        incomeRepository.save(incomeToBeSaved);
-        return incomeValue;
-    }
-
-    //ToDo To be finished
-    @GetMapping("/last_five")
-    public List<Income> getLastFive(){
-        return incomeRepository.findLastFiveRecords();
+    /**
+     * gets last incomes
+     * @param lastNumber - how many incomes should be returned
+     * @return incomes
+     */
+    @GetMapping("/income/{lastNumber}")
+    public List<BigDecimal> getLastIncomes(@PathVariable int lastNumber) {
+        return repository.getLastIncomes(lastNumber);
     }
 }
