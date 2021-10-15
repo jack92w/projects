807ed6a1781aa8c12c23e8ea8fda152dd9230f0c
jacek <jacek.hrynczyszyn@gmail.com>
2021-10-15 21:45:50 +0200

    Income class has been prepared

---
+++ Calculator/Calculator/src/main/java/com/hrynczyszyn/recruitment/calculator/income/Income.java
@@ -0,0 +1,13 @@
+package com.hrynczyszyn.recruitment.calculator.income;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class Income {
+    private BigDecimal incomeValue;
+}
